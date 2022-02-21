1289614780923b3a2f581f865c0886ddaadba519
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-02-22 00:58:11 +0300

    added menu components

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <base-wrapper>
     <div class="home">
       <banner/>
+      <app-menu id="menu"/>
     </div>
   </base-wrapper>
 </template>
@@ -9,14 +10,14 @@
 <script>
 import BaseWrapper from "~/components/common/baseWrapper/BaseWrapper";
 import Banner from "~/components/home/banner/Banner";
+import AppMenu from "~/components/home/menu/AppMenu";
+import {productService} from "~/components/products/products.service";
 
 export default {
   name: 'IndexPage',
-  components: { Banner, BaseWrapper },
-  computed: {
-
-  },
-  methods: {
+  components: { Banner, BaseWrapper, AppMenu },
+  created() {
+    productService.getAll(this)
   }
 }
 </script>
