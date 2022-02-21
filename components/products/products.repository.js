1256aa636fe1a4e44219c4157026f03835c8a5c8
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-02-22 00:57:02 +0300

    added products repository, service, store

---
+++ components/products/products.repository.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export class ProductsRepository {
+
+    async getList(categoryId) {
+        const response = await axios.get(`/api/products`,
+            { params: { category: categoryId } })
+        return response.data
+    }
+
+}
