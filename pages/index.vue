b14f2607b8cea77ced2af8b5f86dc55884edac22
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-02-21 19:34:06 +0300

    added home page banner; added baseWrapper

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <Tutorial/>
+  <base-wrapper>
+    <div class="home">
+      <banner/>
+    </div>
+  </base-wrapper>
 </template>
 
 <script>
+import BaseWrapper from "~/components/common/baseWrapper/BaseWrapper";
+import Banner from "~/components/home/banner/Banner";
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { Banner, BaseWrapper },
+  computed: {
+
+  },
+  methods: {
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.home { width: 100vw; }
+</style>
