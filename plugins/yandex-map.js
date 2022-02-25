2da51f9520b0afd8c7629466092db6d2e2a5b35d
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-02-25 23:43:38 +0300

    refactoring map

---
+++ plugins/yandex-map.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import YmapPlugin from 'vue-yandex-maps'
+
+const settings = {
+  apiKey: '8de247b8-c5f2-40b2-8a90-09fb4bdac04b',
+  lang: 'ru_RU',
+  coordorder: 'latlong',
+  enterprise: false,
+  version: '2.1'
+}
+
+Vue.use(YmapPlugin, settings)
