90a9ac18aee0882780a2a34c53bf8861ea950872
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-02-26 18:08:18 +0300

    fix(product/_id.vue, ProductIngredients.vue, ProductMedia.vue): refactoring product page

---
+++ components/productPage/ProductIngredients.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="product-ingredients">
+    <label>Состав</label>
+    <div class="product-ingredients__list" ref="list">
+      <template v-for="item in product.ingredients">
+        <span>{{item}}</span>
+        <i ref="points"/>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'product-ingredients',
+  props: {
+    product: { type: Object, required: true }
+  },
+  data() {
+    return {
+      callCount: 0
+    }
+  },
+  computed: {
+    windowWidth() {
+      return this.$store.state.windowWidth
+    }
+  },
+  watch: {
+    windowWidth() {
+      this.checkPointsOffset()
+    }
+  },
+  mounted() {
+    this.checkPointsOffset()
+  },
+  methods: {
+    checkPointsOffset() {
+      this.$refs.points.forEach(item => {
+        item.style.display = 'inline-block'
+        if (item.offsetLeft === 10) {
+          item.style.display = 'none'
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.product-ingredients {
+
+  > label {
+    font-family: Neucha, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 20px;
+
+    text-align: left;
+    letter-spacing: 0.08em;
+
+    color: #000000;
+  }
+
+  &__list {
+    position: relative;
+    margin-top: 6px;
+
+    > span {
+      position: relative;
+
+      font-family: Ubuntu, sans-serif;
+      font-style: normal;
+      font-weight: 300;
+      font-size: 14px;
+      line-height: 16px;
+
+      letter-spacing: 0.08em;
+
+      color: #050505;
+
+      @media screen and (max-width: 768px) {
+        font-size: 12px;
+      }
+    }
+
+    > i {
+      display: inline-block;
+      margin-left: 10px;
+      margin-right: 10px;
+
+      width: 6px;
+      height: 6px;
+
+      border-radius: 50%;
+      background-color: #0C334A;
+
+      &:last-child { display: none!important; }
+    }
+
+  }
+
+}
+</style>
---
+++ components/productPage/ProductMedia.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="product-media">
+    <h1>{{product.title}}</h1>
+    <div class="product-media__image">
+      <img class="image" :src="product.images[0]" alt="">
+      <img class="bg" src="@/assets/images/product/bg.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'product-media',
+  props: {
+    product: { type: Object, required: true }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.product-media {
+  min-width: 400px;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    min-width: unset;
+  }
+
+  > h1 {
+    font-family: Neucha, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 34px;
+    line-height: 37px;
+    letter-spacing: 0.08em;
+
+    color: #000000;
+
+    @media screen and (max-width: 1100px) {
+      font-size: 30px;
+    }
+  }
+
+  &__image {
+    position: relative;
+
+    .image {
+      width: 280px;
+      height: 280px;
+      object-fit: contain;
+      @media screen and (max-width: 480px) {
+        width: 224px;
+      }
+    }
+
+    .bg {
+      position: absolute;
+      top: 130px;
+      //left: 40px;
+      left: 0;
+      right: 0;
+      margin: auto;
+      z-index: -1;
+
+      @media screen and (max-width: 480px) {
+        top: 100px;
+        width: 250px;
+        object-fit: contain;
+      }
+    }
+
+  }
+
+}
+</style>
