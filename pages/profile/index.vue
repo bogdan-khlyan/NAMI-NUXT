c246b44d72471e1a145f141a2d5b33976a493284
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-03-16 00:17:29 +0300

    added SettingsForm.vue component and fix profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -3,20 +3,39 @@
     <page-title
       title-icon="user"
       title="Личные данные"/>
+
+    <div class="profile-info__avatar">
+      <edit-avatar/>
+    </div>
+
+
+    <div class="profile-info__content">
+      <settings-form/>
+    </div>
   </div>
 </template>
 
 <script>
 import PageTitle from "@/components/profile/PageTitle";
+import EditAvatar from "@/components/profile/EditAvatar";
+import ProfileInputWrapper from "@/components/profile/ProfileInputWrapper";
+import BaseInput from "@/components/common/BaseInput";
+
+import SettingsForm from "@/components/profile/SettingsForm";
 
 export default {
   name: 'profile-index',
-  components: { PageTitle }
+  components: { PageTitle, EditAvatar, ProfileInputWrapper, BaseInput, SettingsForm }
 }
 </script>
 
 <style lang="scss" scoped>
 .profile-info {
+  height: 100%;
+
+  &__avatar {
+    margin-top: 20px;
+  }
 
 }
 </style>
