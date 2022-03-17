17179471d0406c454bedcbff4d5b1ca97e32a2e7
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-03-17 19:32:25 +0300

    refactoring

--- src/plugins/api-services.js
+++ src/plugins/api-services.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import {UserInstanceService} from "@/api/userInstance/user-instance.service";
 import {MenuService} from "@/api/menu/menu.service";
+import {OrdersService} from "@/api/orders/orders.service";
 
 export default ({ app }) => {
   Vue.prototype.$userInstance = new UserInstanceService(app)
   Vue.prototype.$menu = new MenuService(app)
+  Vue.prototype.$orders = new OrdersService(app)
 }
