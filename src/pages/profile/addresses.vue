508e663b7e72b46ca1b672678972fc726a123e41
kirill.kotov <txrB7rjWPM>
2022-04-11 18:13:39 +0300

    feat: added info input for addresses

--- src/pages/profile/addresses.vue
+++ src/pages/profile/addresses.vue
@@ -4,7 +4,25 @@
       title-icon="home"
       title="Мои адреса"/>
 
-    <address-input v-for="(address,i) in myAddresses" :number="i" :my-address="address"/>
+    <div>
+
+      <component v-for="(address,i) in myAddresses" :key="i"
+                  :is="typeMode(address)"
+                 :number="i+1" :address-info="address"
+                 @editAddress="editableAddresses.push($event)"/>
+
+    </div>
+
+    <button class="address__btn-add-address" @click="addNewAddress">
+      <img src="@/assets/images/icons/icon-plus.svg" alt="add">
+      Добавить новый адрес
+    </button>
+
+    <button v-if="isEditMode" class="address__btn-save-address" @click="addNewAddress">
+      <img src="@/assets/images/icons/icon-plus.svg" alt="add">
+      Сохранить адреса
+    </button>
+
 
   </div>
 </template>
@@ -12,25 +30,50 @@
 <script>
 import PageTitle from "@/components/pages/profilePage/common/PageTitle";
 import AddressInput from "@/components/pages/profilePage/addresses/AddressInput";
+import AddressInfo from "@/components/pages/profilePage/addresses/AddressInfo";
+
 export default {
   name: 'addresses',
-  components: { PageTitle, AddressInput },
-  data(){
+  components: {PageTitle, AddressInput, AddressInfo},
+  data() {
     return {
-      myAddresses:[
+      myAddresses: [
         {
-          city:'',
-          street:'ул. Заперевальная, д.283, кв.3'
+          id: 0,
+          city: '',
+          street: 'ул. Заперевальная, д.283, кв.3'
         },
         {
-          city:'',
-          street:'ул.Шахтерской Дивизии, д.39'
+          id: 1,
+          city: '',
+          street: 'ул.Шахтерской Дивизии, д.39'
         },
         {
-          city:'',
-          street:''
+          id: 2,
+          city: '',
+          street: ''
         },
-      ]
+      ],
+      editableAddresses: []
+    }
+  },
+  computed:{
+    isEditMode(){
+      return this.editableAddresses.length > 0 ||
+        this.myAddresses.filter(address => address.id === undefined).length>0
+    }
+  },
+  methods: {
+
+    typeMode(address){
+      return this.editableAddresses.includes(address.id) || !address.hasOwnProperty('id') ?'address-input':'address-info'
+    },
+    saveForm() {
+      alert('save')
+    },
+    addNewAddress() {
+      alert('addNewAddress')
+      this.myAddresses.push({city:'',street:''})
     }
   }
 }
@@ -38,9 +81,84 @@ export default {
 
 <style lang="scss" scoped>
 .address {
+  max-width: 500px;
 
   .page-title {
     margin-bottom: 18px;
+  }
+
+  &__btn-add-address {
+    margin-top: 44px;
+    margin-bottom: 24px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: Ubuntu, sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #FFFFFF;
+
+    width: 100%;
+    height: 56px;
+
+    background: #062D4E;
+    border-radius: 4px;
+    border: none;
+
+    cursor: pointer;
+    transition: 200ms;
+
+    img {
+      margin-right: 10px;
+    }
+
+    @media screen and (max-width: 420px) {
+      width: calc(100vw - 20px);
+    }
+
+    &:hover {
+      background: #0e508a;
+    }
+  }
+
+  &__btn-save-address {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-family: Ubuntu, sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #062D4E;
+
+    width: 100%;
+    height: 56px;
+
+    background: #FFFFFF;
+    border-radius: 4px;
+    border: 1px solid #062D4E;
+
+    cursor: pointer;
+    transition: 200ms;
+
+    img {
+      margin-right: 10px;
+    }
+
+    @media screen and (max-width: 420px) {
+      width: calc(100vw - 20px);
+    }
+
+    &:hover {
+      background: #062D4E;
+      color: #FFFFFF;
+    }
   }
 }
 </style>
