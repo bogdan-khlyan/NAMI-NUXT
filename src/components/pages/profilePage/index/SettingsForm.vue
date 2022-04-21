c724b46cd129e223eb3a6e05fc52afefff7954a0
kirill.kotov <txrB7rjWPM>
2022-04-21 14:04:34 +0300

    added edit input for telegram on page profile edit

--- src/components/pages/profilePage/index/SettingsForm.vue
+++ src/components/pages/profilePage/index/SettingsForm.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="settings-form">
+
     <h2>Настройки аккаунта</h2>
+
     <div class="settings-form__item">
       <profile-input-wrapper
         :value="form.phoneNumber"
@@ -11,10 +13,11 @@
           v-model="form.phoneNumber"
           v-mask="'+38(071)-###-##-##'"
           :loading="loading.phone"
-          label="Имя"
-          placeholder="Вы еще не указали свое имя"/>
+          label="Номер телефона"
+          placeholder="Вы еще не указали свой номер"/>
       </profile-input-wrapper>
     </div>
+
     <div class="settings-form__item">
       <profile-input-wrapper
         :value="form.username"
@@ -30,6 +33,7 @@
           placeholder="Вы еще не указали свое имя"/>
       </profile-input-wrapper>
     </div>
+
     <div class="settings-form__item">
       <profile-input-wrapper
         :value="form.email"
@@ -39,12 +43,54 @@
         @submit="submitEmail"
         ref="emailInputWrapper">
         <base-input
-          v-model="form.username"
-          :loading="loading.userName"
-          label="Имя"
-          placeholder="Вы еще не указали свое имя"/>
+          v-model="form.email"
+          :loading="loading.email"
+          label="Email"
+          placeholder="Вы еще не указали свой email"/>
       </profile-input-wrapper>
     </div>
+
+    <div class="settings-form__item settings-form__item-telegram">
+      <profile-input-wrapper
+        :value="form.telegram"
+        label="Telegram"
+        placeholder="Вы еще не указали свой аккаунт"
+        :loading="loading.telegram"
+        @submit="submitTelegram"
+        ref="emailInputTelegram">
+        <base-input
+          v-model="form.telegram"
+          :loading="loading.telegram"
+          label="Telegram"
+          placeholder="Вы еще не указали свой аккаунт"/>
+
+        <div class="help">
+          <div class="help-title">Для подключения уведомлений вам нужно:</div>
+          <ol class="help-list">
+            <li>Зайти в указанный аккаунт телеграма</li>
+            <li>Найти бота <span>@namisushi_bot</span></li>
+            <li>Написать боту <span>/start</span></li>
+          </ol>
+        </div>
+
+      </profile-input-wrapper>
+
+      <img class="icon-telegram" src="@/assets/images/icons/icon-telegram.svg" alt="icon">
+      <el-tooltip popper-class="tooltip-edit-telegram" effect="dark" placement="right-start">
+        <img class="icon-help" src="@/assets/images/icons/icon-help.svg" alt="icon">
+        <div slot="content">
+          <div class="tooltip-title">Для чего нужен telegram?</div>
+          <div class="tooltip-description">
+            После привязки телеграм аккаунта к боту приложения
+            NamiSushi - вам будут приходить уведомления о статусе заказа
+          </div>
+          <div class="tooltip-description">
+            * Для привязки телеграм аккаунта у вас должен быть линк - сслыка на ваш профиль
+          </div>
+        </div>
+      </el-tooltip>
+    </div>
+
   </div>
 </template>
 
@@ -63,12 +109,14 @@ export default {
       loading: {
         phone: false,
         userName: false,
-        email: false
+        email: false,
+        telegram: false
       },
       form: {
         phoneNumber: '+38(071)-700-97-91',
         username: null,
-        email: null
+        email: null,
+        telegram: null
       }
     }
   },
@@ -82,23 +130,31 @@ export default {
       }, 3000)
     },
     submitEmail() {
-      console.log('submit email')
       this.loading.email = true
       setTimeout(() => {
         this.loading.email = false
         this.$refs.emailInputWrapper.setEditMode(false)
       }, 3000)
+    },
+    submitTelegram() {
+      this.loading.telegram = true
+      setTimeout(() => {
+        this.loading.telegram = false
+        this.$refs.emailInputTelegram.setEditMode(false)
+      }, 1000)
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .settings-form {
   width: max-content;
+  margin-bottom: 72px;
 
   h2 {
     margin-top: 32px;
+    margin-bottom: 0;
 
     font-family: Ubuntu, sans-serif;
     font-style: normal;
@@ -110,9 +166,84 @@ export default {
   }
 
   &__item {
-    margin-top: 12px;
-    margin-bottom: 12px;
+    position: relative;
+    margin: 24px 0;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  &__item-telegram {
+    .profile-input-wrapper__label, .password-input__label {
+      margin-left: 24px;
+    }
+
+    .help-title {
+      font-family: Ubuntu, sans-serif;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 20px;
+      text-align: left;
+      color: #062D4E;
+      margin: 4px 0;
+    }
+    .help-list {
+      font-family: Ubuntu, sans-serif;
+      font-size: 14px;
+      font-weight: 300;
+      line-height: 20px;
+      text-align: left;
+      color: #121212;
+
+      margin: 0;
+      padding-left: 16px;
+      span {
+        font-weight: 400;
+      }
+    }
+  }
+  .icon-telegram {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .icon-help {
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: pointer;
   }
 
+}
+
+.tooltip-edit-telegram {
+  max-width: 234px;
+  padding: 6px 10px;
+  box-sizing: border-box;
+  background: #051523 !important;
+
+  .tooltip-title {
+    font-family: Ubuntu, sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 16px;
+    text-align: left;
+    color: #FFFFFF;
+    word-break: break-word;
+  }
+
+  .tooltip-description {
+    font-family: Ubuntu, sans-serif;
+    font-size: 12px;
+    font-weight: 300;
+    line-height: 16px;
+    text-align: left;
+    color: #FFFFFF;
+    word-break: break-word;
+
+    &:last-child {
+      margin-top: 16px;
+    }
+  }
 }
 </style>
