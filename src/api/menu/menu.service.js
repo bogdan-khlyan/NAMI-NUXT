e88c79d967f462529077367791caa29f228571ec
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-10-03 19:38:04 +0300

    fix: api

--- src/api/menu/menu.service.js
+++ src/api/menu/menu.service.js
@@ -1,35 +1,21 @@
-import {MenuRepository} from "@/api/menu/menu.repository";
-
-export class MenuService {
-
-  #store = null
-  #router = null
-  #repository = new MenuRepository()
-
-  constructor(app) {
-    this.#store = app.store
-    this.#router = app.router
-  }
-
+export default ({ $axios, store, router, $toast }) => ({
   async getMenu() {
-    const menu = await this.#repository.getMenu()
-
+    const menu = await $axios.$get('/api/products')
     const result = {
       ...menu,
-      products: menu.products.map(product => ({
-        ...product,
-        $id: product.title
-          .toLowerCase()
-          .replaceAll(' ', '-'),
-        images: product.images.map(image => '/api/product/image/' + image)
-      }))
+      products: menu.products.map(product => {
+        return {
+          ...product,
+          $id: product.title
+            .toLowerCase()
+            .replaceAll(' ', '-'),
+          images: product.images.map(image => '/api/product/image/' + image)
+        }
+      })
     }
 
-    this.#store.commit('menu.setMenu', result)
+    store.commit('menu.setMenu', result)
 
     return result
   }
-
-}
-
-export default {}
+})
