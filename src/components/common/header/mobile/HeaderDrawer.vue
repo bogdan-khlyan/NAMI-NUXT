0911dc408b2ef68c21050e4152e9f930cc77699d
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-11-12 02:24:32 +0300

    fix(HeaderDrawer.vue): css

--- src/components/common/header/mobile/HeaderDrawer.vue
+++ src/components/common/header/mobile/HeaderDrawer.vue
@@ -228,8 +228,8 @@ export default {
     top: 0;
     left: 0;
 
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
 
     background: rgba(60, 75, 97, 0.6);
     backdrop-filter: blur(10px);
