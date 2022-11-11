c6b4629f0370bb6cf02a75132bc838919b83c18f
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2022-11-11 15:07:16 +0300

    fix(base-error.js): message field

--- src/utils/base-error.js
+++ src/utils/base-error.js
@@ -1,6 +1,6 @@
 export function baseError(error, $toast, baseMessage = 'Что-то пошло не так...') {
-  if (error?.response?.data?.message) {
-    $toast?.error(error.response.data.message)
+  if (error?.response?.data?.error?.message) {
+    $toast?.error(error.response.data.error.message)
   } else if (baseMessage) {
     $toast?.error(baseMessage)
     return baseMessage
